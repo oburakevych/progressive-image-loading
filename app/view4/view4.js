5f983ccc3d766ec417b9e7ce2d731ff944e52b7a
oburakevych <oburakevych@gmail.com>
2014-09-21 20:49:32 +1000

    added new test image in jpg

---
+++ app/view4/view4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.view4', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view4', {
+    templateUrl: 'view4/view4.html',
+    controller: 'View4Ctrl'
+  });
+}])
+
+.controller('View4Ctrl', [function() {
+	var image = new Image();
+	image.onload = function() {
+		var imageElement = angular.element(document.getElementById("noImage4"));
+		imageElement.attr('src', image.src);
+	}
+
+	image.src = "https://raw.githubusercontent.com/oburakevych/progressive-image-loading/master/app/images/avatar-1260x1600x100.jpg";
+}]);
